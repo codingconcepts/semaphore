b2d66a2af1dcfe9d733b1cee3637115d6c9b1553
Rob Reid <robreid@crlMBP-NHJNH36CFJMjY2.local>
2023-02-26 20:26:41 +0000

    feat: Export semaphore struct

--- semaphore.go
+++ semaphore.go
@@ -1,20 +1,20 @@
 package semaphore
 
-// semaphore is a channel-backed construct allowing N operations
+// Semaphore is a channel-backed construct allowing N operations
 // to happen concurrently.
-type semaphore struct {
+type Semaphore struct {
 	c chan struct{}
 }
 
 // New returns a pointer to a new instance of semaphore.
-func New(concurrency int) (s *semaphore) {
-	return &semaphore{
+func New(concurrency int) (s *Semaphore) {
+	return &Semaphore{
 		c: make(chan struct{}, concurrency),
 	}
 }
 
 // Run executes a function, blocking if N operations are executing.
-func (s *semaphore) Run(f func()) {
+func (s *Semaphore) Run(f func()) {
 	// Try to add to the channel and block if it's full.
 	s.c <- struct{}{}
 
@@ -36,7 +36,7 @@ func (s *semaphore) Run(f func()) {
 // kicking off and are running after having kicked them all off.
 // If you call this part way through your operations, there's no
 // guaranteeing all of the operations would have finished.
-func (s *semaphore) Wait() {
+func (s *Semaphore) Wait() {
 	// Fill the channel with empty structs, ensuring that there are
 	// no existing operations running, as these would need to finish
 	// before the channel could be written to.
